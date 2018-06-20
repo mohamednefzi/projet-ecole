27b72ef4d3711f54b7ba19ad01359f2d264e8d75
Nefzi <mohamed.nefzi@cgi.com>
2018-06-20 08:42:52 -0400

    update commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import { ModalTermsAndConditionsComponent} from './modals/modal-terms-and-conditions/modal-terms-and-conditions.component.';
+import { ModalLoginComponent } from './modals/modal-login/modal-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomePageComponent,
+    ModalTermsAndConditionsComponent,
+    ModalLoginComponent,
   ],
   imports: [
     BrowserModule
